103cfa7e2dde544e237cd8367e48e5ffbb38216b
dedenbangkit <mail@dedenbangkit.com>
2023-01-18 17:51:34 +0700

    [#1] Example csv output

--- presentation-180123-maep-akvo-flow-api/code/AkvoFlowExample/src/main/java/DataHandler/Folder.java
+++ presentation-180123-maep-akvo-flow-api/code/AkvoFlowExample/src/main/java/DataHandler/Folder.java
@@ -4,8 +4,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javax.lang.model.element.Element;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 public class Folder {
@@ -14,33 +13,41 @@ public class Folder {
         this.data = data.getJSONArray("folders");
     }
     public void print() throws IOException {
-        File file = new File("./folder-list.csv");
+        if (this.data.length() == 0) {
+            System.out.println("NO Folders Available");
+            System.exit(0);
+        }
+        File file = new File("./output.csv");
         if (file.createNewFile()) {
             System.out.println("File created: " + file.getName());
         } else {
             System.out.println("File already exists.");
         }
-        if (this.data.length() == 0) {
-            System.out.println("NO Folders Available");
-            System.exit(0);
-        }
+        FileOutputStream fos = new FileOutputStream(file);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
         JSONObject firstObject = (JSONObject)  this.data.get(0);
         Object[] headers = (Object[]) firstObject.keySet().toArray();
         for (int i = 0; i < Objects.requireNonNull(headers).length; i++) {
-            System.out.print(headers[i]);
+            // System.out.print(headers[i]);
+            bw.write((String) headers[i]);
             if (i != headers.length - 1) {
-                System.out.print(",");
+                bw.write(",");
+                // System.out.print(",");
             }
         }
         for (Object datum : this.data) {
             JSONObject ds = (JSONObject) datum;
-            System.out.println();
+            bw.newLine();
+            // System.out.println();
             for (int i = 0; i < Objects.requireNonNull(headers).length; i++) {
-                System.out.print(ds.get((String) headers[i]));
+                // System.out.print(ds.get((String) headers[i]));
+                bw.write((String) ds.get((String) headers[i]));
                 if (i != headers.length - 1) {
-                    System.out.print(",");
+                    // System.out.print(",");
+                    bw.write(",");
                 }
             }
         }
+        bw.close();
     }
 }
